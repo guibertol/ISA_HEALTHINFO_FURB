f0044db4f5415ae0fc421bcef49e7a3b73e946e6
Guilherme Bertol <guilherme.bertol@hotmail.com>
2021-06-28 20:55:20 -0300

    Implementado tela de ajuda ao usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'help',
+    loadChildren: () => import('./help/help.module').then( m => m.HelpPageModule)
   }
+
 ];
 @NgModule({
   imports: [
